19bb1def7556f52d4256a87e0e72de770e63feec
Jackson Pinchot <pinchojd@mail.uc.edu>
2025-04-03 14:57:45 -0400

    updating sidebar

--- src/pages/sidebar.jsx
+++ src/pages/sidebar.jsx
@@ -1,67 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import sidebarImage from '../assets/house-icon.png';
+import LoginModal from '../components/LoginModal';
 
 
 function Sidebar() {
-  // State to track whether the sidebar is toggled
   const [isToggled, setIsToggled] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
-  // Check if the sidebar toggle state is saved in localStorage
-  useEffect(() => {
-    const savedToggleState = localStorage.getItem('sb|sidebar-toggle');
-    if (savedToggleState === 'true') {
-      setIsToggled(true);
-    }
-  }, []);
+  const handleLoginClick = () => {
+    setShowLoginModal(true);  // Show the login modal
+    setIsToggled(false);     // Close the sidebar
+  };
+  const handleCloseModal = () => setShowLoginModal(false);
 
-  // Apply the sb-sidenav-toggled class to the body if the sidebar is toggled
-  useEffect(() => {
-    if (isToggled) {
-      document.body.classList.add('sb-sidenav-toggled');
-    } else {
-      document.body.classList.remove('sb-sidenav-toggled');
-    }
-    // Save the new state to localStorage
-    localStorage.setItem('sb|sidebar-toggle', isToggled);
-  }, [isToggled]);
-
-  // Function to toggle the sidebar
   const toggleSidebar = () => {
     setIsToggled(prevState => !prevState);
   };
 
+  useEffect(() => {
+    const savedToggleState = localStorage.getItem('sb|sidebar-toggle');
+    if (savedToggleState === 'true') {
+      setIsToggled(true);
+    } else {
+      setIsToggled(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('sb|sidebar-toggle', isToggled);
+  }, [isToggled]);
+
   return (
     <div className={`d-flex ${isToggled ? 'sb-sidenav-toggled' : ''}`} id="wrapper">
-      {/* Sidebar */}
-      <div className={`border-end bg-white ${isToggled ? 'sb-sidenav-toggled' : ''}`} id="sidebar-wrapper">
-        <div className="sidebarTitleContainer">
-          <div className="sidebar-heading border-bottom bg-light sidebarTitle">Course Home<img className="sidebarImage" src={sidebarImage} alt="icon image of a house" /></div>
-        </div>
-        <div className="list-group list-group-flush">
-          <Link to="/course-home" id="sidebar" className="list-group-item list-group-item-action list-group-item-light p-3 sidebar sidebarLink">Course Home</Link>
-          <Link to="/cooking" id="cooking" className="list-group-item list-group-item-action list-group-item-light p-3">Cooking</Link>
-          <Link to="/produce" id="produce" className="list-group-item list-group-item-action list-group-item-light p-3">Produce</Link>
-          <Link to="/sautee" id="sautee" className="list-group-item list-group-item-action list-group-item-light p-3">Sautee</Link>
-          <Link to="/sear" id="sear" className="list-group-item list-group-item-action list-group-item-light p-3">Sear</Link>
-          <Link to="/international" id="sear" className="list-group-item list-group-item-action list-group-item-light p-3">International</Link>
+      <div id="sidebar-wrapper" className={isToggled ? "open" : ""}>
+        {/* Sidebar content here */}
+        <div className={`border-end bg-white ${isToggled ? 'sb-sidenav-toggled' : ''}`} id="sidebar-content">
+          <div className="sidebarTitleContainer">
+            <div className="sidebar-heading border-bottom bg-light sidebarTitle">
+              Course Home<img className="sidebarImage" src={sidebarImage} alt="icon image of a house" />
+            </div>
+          </div>
+          <div className="list-group list-group-flush">
+            <Link to="/course-home" id="sidebar" className="list-group-item list-group-item-action list-group-item-light p-3 sidebar sidebarLink">Course Home</Link>
+            <Link to="/cooking" id="cooking" className="list-group-item list-group-item-action list-group-item-light p-3">Cooking</Link>
+            <Link to="/produce" id="produce" className="list-group-item list-group-item-action list-group-item-light p-3">Produce</Link>
+            <Link to="/sautee" id="sautee" className="list-group-item list-group-item-action list-group-item-light p-3">Sautee</Link>
+            <Link to="/sear" id="sear" className="list-group-item list-group-item-action list-group-item-light p-3">Sear</Link>
+            <Link to="/international" id="sear" className="list-group-item list-group-item-action list-group-item-light p-3">International</Link>
+          </div>
         </div>
       </div>
 
       {/* Page content wrapper */}
-      <div id="page-content-wrapper">
+      <div id="page-content-wrapper" className={isToggled ? 'shifted' : ''}>
         {/* Top navigation */}
         <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom navbarContainer">
           <div className="container-fluid">
-            <button className="hamburger-btn" id="sidebarToggle" onClick={toggleSidebar}>
-              <div className={`hamburger ${!isToggled ? 'toggled' : ''}`}>
+            <button
+              className={`hamburger-btn ${isToggled ? 'right' : ''}`}
+              id="sidebarToggle"
+              onClick={toggleSidebar}
+            >
+              <div className={`hamburger ${isToggled ? 'toggled' : ''}`}>
                 <div className="line line-1"></div>
                 <div className="line line-2"></div>
                 <div className="line line-3"></div>
               </div>
             </button>
             <div className="navbarTitleContainer">
-              <h1 className='mt-4 navbarTitle'><Link to="/" id="home" className="navbarTitleLink">Video &#10140; Article &#10140; Quiz &#10140; Profile &#10140; Status </Link></h1>
+              <h1 className='mt-4 navbarTitle'>
+                <Link to="/" id="home" className="navbarTitleLink">
+                  Video &#10140; Article &#10140; Quiz &#10140; Profile &#10140; Status
+                </Link>
+              </h1>
             </div>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
@@ -69,51 +81,23 @@ function Sidebar() {
                 <li className="nav-item"><a className="nav-link" href="#!">Link</a></li>
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                  <div className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <div className="dropdown-menu dropdown-menu-end" aria-labelled="navbarDropdown">
                     <a className="dropdown-item" href="#!">Action</a>
                     <a className="dropdown-item" href="#!">Another action</a>
                     <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#!">Something else here</a>
+                    <button
+                      className="dropdown-item"
+                      onClick={handleLoginClick}
+                      id="login-button"
+                    >
+                      Login
+                    </button>
                   </div>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
-
-        {/* Courses Section */}
-              <section id="courses" className="container mt-5">
-                <h2>Courses</h2>
-                <div className="row">
-                  <div className="col-md-4">
-                    <div className="card mb-4">
-                      <img src="https://images.unsplash.com/photo-1556911220-dabc1f02913a?q=80&w=2070&auto=format&fit=crop" className="card-img-top" alt="Produce Basics" />
-                      <div className="card-body">
-                        <h5 className="card-title">Produce Basics</h5>
-                        <Link to="/course-home" className="btn btn-primary">Start Learning</Link>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="card mb-4">
-                      <img src="https://images.unsplash.com/photo-1504693390394-c24671c3e8bb?q=80&w=2070&auto=format&fit=crop" className="card-img-top" alt="Searing Basics" />
-                      <div className="card-body">
-                        <h5 className="card-title">Searing Basics</h5>
-                        <Link to="/course-home" className="btn btn-primary">Start Learning</Link>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="card mb-4">
-                      <img src="https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?w=500&auto=format&fit=crop&q=60" className="card-img-top" alt="Sautee Basics" />
-                      <div className="card-body">
-                        <h5 className="card-title">Sautee Basics</h5>
-                        <Link to="/course-home" className="btn btn-primary">Start Learning</Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
 
         {/* Page content */}
         <div className="container-fluid">
@@ -127,6 +111,9 @@ function Sidebar() {
             ID which will toggle the menu when clicked.
           </p>
         </div>
+
+        {/* Login Modal */}
+        <LoginModal show={showLoginModal} onClose={handleCloseModal} />
       </div>
     </div>
   );
