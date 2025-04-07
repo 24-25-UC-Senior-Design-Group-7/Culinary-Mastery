9f918be273c79ea76a400a17e6d966e2ce7bf520
Jackson Pinchot <pinchojd@mail.uc.edu>
2025-04-07 11:19:16 -0400

    css updates

--- src/pages/sear.jsx
+++ src/pages/sear.jsx
@@ -34,11 +34,11 @@ function Sear() {
   }, [updateSidebarProps]);
 
   return (
-    <>
+    <div className="content-wrapper">
       <h1 className="mt-4">Sear Techniques</h1>
       <p>Learn how to sear meat and other ingredients to perfection in this section. Searing enhances flavors and adds texture.</p>
       <p>Explore different methods for searing, from pan-searing to grilling, and master this important cooking technique.</p>
-    </>
+    </div>
   );
 }
 
